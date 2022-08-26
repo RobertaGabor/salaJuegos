53175ce4640dadd616532f40ed3bef66bf36356b
Roberta Gabor <robyrubirex@gmail.com>
2022-08-25 22:43:34 -0300

    add user

---
+++ src/app/Vistas/juego/juego.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/Entidades/usuario';
+
+@Component({
+  selector: 'app-juego',
+  templateUrl: './juego.component.html',
+  styleUrls: ['./juego.component.css']
+})
+export class JuegoComponent implements OnInit {
+
+  public user:Usuario = new Usuario();
+  public user2:Usuario|undefined;
+  constructor() { 
+    this.user.nombre="";
+  }
+
+  ngOnInit(): void {
+    this.user.nombre="Roby";
+    console.log(this.user.nombre);
+    this.user2=new Usuario();
+  }
+
+  cambiarNombre():void{
+    this.user.nombre="Lara";
+    this.user.apellido="peposo";
+  }
+  aceptar():void{
+    console.log(this.user.nombre);
+    console.log(this.user.apellido);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JuegoComponent } from './Vistas/juego/juego.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //hace el cambhio de ts  y html automatico <input type="text" [(ngModel)]="apellido">
   ],
   providers: [],
   bootstrap: [AppComponent]
